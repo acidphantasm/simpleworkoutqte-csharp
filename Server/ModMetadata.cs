f82c0ec6c64cb83e6998453f05fec8ab08795ceb
acidphantasm <acidphantasm@users.noreply.github.com>
2026-08-29 15:42:08 -0500

    4.1.3+

--- Server/ModMetadata.cs
+++ Server/ModMetadata.cs
@@ -9,7 +9,7 @@ public record ModMetadata : IModMetadata
     public string Author { get; init; } = "acidphantasm";
     public List<string>? Contributors { get; init; }
     public SemanticVersioning.Version Version { get; init; } = new("2.2.1");
-    public SemanticVersioning.Range SptVersion { get; init; } = new("~4.1.0");
+    public SemanticVersioning.Range SptVersion { get; init; } = new("~4.1.3");
     public List<string>? Incompatibilities { get; init; }
     public Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
     public string? Url { get; init; }
